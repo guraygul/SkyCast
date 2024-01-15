5eb33d9601382a23c6ac0934bef188ce02d2cbb3
Güray Gül <guraygul59@gmail.com>
2024-01-15 23:27:08 +0300

    Initial Commit

---
+++ SkyCast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkyCast
+//
+//  Created by Güray Gül on 15.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
